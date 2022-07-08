2515e9e13343c1ab08a093427c8765c458235be8
Joseph Wilt <josephwilt13@gmail.com>
2022-07-08 11:01:08 -0500

    update

--- Learning Coding/3 Lists.py
+++ Learning Coding/3 Lists.py
@@ -255,3 +255,160 @@ print(customer_data)
 new_customer_data = [["Amit", "Large", True], ["Karim", "X-Large", False]]
 customer_data_final = customer_data + new_customer_data
 print(customer_data_final)
+
+
+
+#WORKING WITH LISTS
+#Python List Methods That Will Be Discussed
+#.count() Count the number of occurrences of an element in a list
+#.insert() Insert an element into a specific index
+#.pop() Remove an element from a specifi index or at the end of a list
+#range() Built in python function to create a sequence of integers
+#len() Built in python function to get the length of a list
+#.sort() / sorted() A method and built in function to sort a list
+
+
+#ADDING BY INDEX: INSERT
+# .insert() allows us to add an element to a specific index in a list.
+# takes two inputs:
+# the index you want to insert to and the element you want to insert at the specified index
+# .insert() method will handle shifting over elements and can be used with negative indices
+# To see it in action let's imagine we have a list representing a line at a store:
+
+store_line = ["Karla", "Maxium", "Martim", "Isabella"]
+
+#Maxim saved a spot for his friend Viktor and we need to adjust the list to add him into the line
+#For this example, we can assume Karla is the front of the line and the rest of the elements are behind her
+#Here is how we would use the .insert() method to insert Viktor:
+
+store_line.insert(2, "Vikor")
+
+#The order and number of the inputs is important. The .insert() method expects two inputs, 
+#the first being a numerical index, followed by any value as the second input.
+#When we insert an element into a list, 
+#all elements from the specified index and up to the last index are shifted one index to the right.
+
+#Example problem:
+#Starting code:
+front_display_list = ["Mango", "Filet Mignon", "Chocolate Milk"]
+print(front_display_list)
+
+front_display_list.insert(0, "Pineapple")
+print(front_display_list)
+
+
+#REMOVING BY INDEX: POP
+#Python gives us a method to remove elements at a specific index using a method called .pop().
+#The .pop() method takes an optional single input:
+#The index for the element you want to remove.
+
+#To see it in action, let’s consider a list called cs_topics 
+# that stores a collection of topics one might study in a computer science program.
+
+cs_topics = ["Python", "Data Structures", "Balloon Making", "Algorithms", "Clowns 101"]
+
+#Two of these topics don’t look like they belong, let’s see how we remove them using .pop().
+
+removed_element = cs_topics.pop()
+print(cs_topics)
+print(removed_element)
+
+#The method can be called without a specific index.
+#Using .pop() without an index will remove whatever the last element of the list is.
+#.pop() is unique in that it will return the value that was removed. 
+#If we wanted to know what element was deleted, simply assign a variable to the call of the .pop() method. 
+#In this case, we assigned it to removed_element.
+
+#Lastly lets remove "Balloon Making"
+
+cs_topics.pop(2)
+print(cs_topics)
+
+#The method can be called with an optional specific index to remove. 
+#In our case, the index 2 removes the value of "Balloon Making"
+#We don’t have to save the removed value to any variable if we don’t care to use it later.
+
+#Example problem
+#Starting code:
+
+data_science_topics = ["Machine Learning", "SQL", "Pandas", "Algorithms", "Statistics", "Python 3"]
+print(data_science_topics)
+
+#It looks like we have an overlap with our computer science curriculum for the topic of "Python 3".
+#Let’s remove the topic from the list of data_science_topics using our newly learned .pop() method.
+
+#Print data_science_topics to see your result.
+
+data_science_topics.pop()
+print(data_science_topics)
+
+#Looks like there is overlap on the "Algorithms" topic as well. Let’s use .pop() to remove it as well.
+#Print data_science_topics to see the changes.
+
+data_science_topics.pop(3)
+print(data_science_topics)
+
+
+#CONSECUTIVE LISTS: RANGE
+#Often, we want to create a list of consecutive numbers in our programs. 
+#For example, suppose we want a list containing the numbers 0 through 9:
+
+my_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+#Typing out all of those numbers takes time and the more numbers we type, 
+#the more likely it is that we have a typo that can cause an error.
+#The function range() takes a single input, 
+#and generates numbers starting at 0 and ending at the number before the input.
+#So, if we want the numbers from 0 through 9, we use range(10) because 10 is 1 greater than 9
+
+my_range = range(10)
+print(my_range)
+
+#The range() function is unique in that it creates a range object. 
+#It is not a typical list like the ones we have been working with.
+#In order to use this object as a list, 
+#we have to first convert it using another built-in function called list().
+#The list() function takes in a single input for the object you want to convert.
+#We use the list() function on our range object like this:
+
+print(list(my_range))
+
+#Example Problem:
+#Starting Code:
+number_list = range(3)
+print(list(number_list))
+
+#Modify number_list 
+#so that it is a range containing numbers starting at 0 and up to, but not including, 9.
+
+number_list = range(9)
+print(list(number_list))
+
+#Create a range called zero_to_seven with the numbers 0 through 7.
+#Print the result in list form.
+
+zero_to_seven = range(8)
+print(list(zero_to_seven))
+
+#By default, range() creates a list starting at 0
+#However, if we call range() with two inputs, we can create a list that starts at a different number.
+#For example, range(2, 9) would generate numbers starting at 2 and ending at 8 (just before 9):
+
+my_list = range(2, 9)
+print(list(my_list))
+
+#If we use a third input, we can create a list that “skips” numbers.
+#For example, range(2, 9, 2) will give us a list where each number is 2 greater than the previous number:
+#lists a range from two through nine but every number gets added by 2
+
+my_range2 = range(2, 9, 2)
+print(list(my_range2))
+
+#We can skip as many numbers as we want!
+#For example, we’ll start at 1 and skip in increments of 10 between each number until we get to 100:
+
+my_range3 = range(1, 100, 10)
+print(list(my_range3))
+
+#Our list stops at 91 because the next number in the sequence would be 101, 
+#which is greater than 100 (our stopping point).
